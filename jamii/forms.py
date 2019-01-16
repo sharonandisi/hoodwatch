b068aa1eee9332d78b4a7af27dc2a248fb26f339
sharonandisi <sharonandisi.28@gmail.com>
2019-01-16 10:21:25 +0300

    create business, neighbourhood, post, comment forms

--- jamii/forms.py
+++ jamii/forms.py
@@ -8,3 +8,22 @@ class ProfileForm(forms.ModelForm):
         exclude = ['user']
 
 class BusinessForm(forms.ModelForm):
+
+    class Meta:
+        model = Business
+        exclude = ['user', 'neighbourhood']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user', 'neighbourhood']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('comment',)
+
+class NeighbourhoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        fields = ('name', 'location', 'occupants')
