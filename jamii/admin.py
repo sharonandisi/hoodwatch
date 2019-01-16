5d93b87b405ce0459c2c2462256d8ddf92f132e2
sharonandisi <sharonandisi.28@gmail.com>
2019-01-16 12:23:03 +0300

    create login.html file to enable user to login

--- jamii/admin.py
+++ jamii/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Neighbourhood, Profile, Post, Location, Comment
 
 # Register your models here.
+admin.site.register(NeighbourHood)
+admin.site.register(UserProfile)
+admin.site.register(Business)
+admin.site.register(Location)
+admin.site.register(Post)
+admin.site.register(Comment)
