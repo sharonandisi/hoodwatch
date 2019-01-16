f29112fc9aa67aa109342a7352f63cbbc746071a
sharonandisi <sharonandisi.28@gmail.com>
2019-01-16 13:05:17 +0300

    create url paths

---
+++ jamii/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url , include
+from django.conf import settings
+from django.conf.urls.static import static 
+from . import views
+
+
+urlpatterns = [
+    url('^$', views.index, name='index'),
+    url(r'^search/', views.search, name='search'),
+    url(r'^profile/(?P<username>\w{0,50})$', views.profile, name='profile'),
+    url(r'^edit_profile/(?P<username>\w{0,50})$', views.edit_profile, name='edit_profile'),
+    url(r'^new_post$', views.new_post, name='new_post'),
+    url(r'^new_business$', views.new_business, name='new_business')
+
+]
